f5f00abee82d70779a6cbfb3b1045110298387d8
ghostwriter <acebedo.mark@gmail.com>
2020-09-03 10:49:23 +0800

    code along

---
+++ Variables_Expressions_and_Statements.jl
@@ -0,0 +1,53 @@
+# This are code-along snippets on Chapter 2. Variables, Expressions and Statements
+message = "And now for something completely different"
+
+n = 17
+
+π_val = 3.141592653589793
+
+# Expressions and Statements
+
+n + 25
+
+# Operator Precedence
+# PEMDAS - Parenthesis, Exponentiation, Multiplication, Division, Addition, Subtraction
+
+
+# String Operations - and some exceptions
+(*), (^)
+
+"Spam"^3
+"Spam"*"Spam"
+
+# Debugging
+1. Syntax error - breaks in the structure and rules
+2. Runtime error - exceptions, appears until program execution start
+3. Semantic error - runs successfully but gives wrong results
+
+# Exercise 2-2
+n = 42
+42 = n # invalid assignment location
+
+x = y = 1 # strangely this works, x, y gets 1
+
+println(n); # semicolon does nothing
+
+1 + 2 ; # with semi-colon, no printout
+
+2 + 2. # returns a float
+
+xy # xy not defined
+
+5x # returns 5 = 5*1
+
+# Exercise 2-3
+# compute the volume of a sphere
+
+π = 3.141592653589793
+r = 5
+
+v = (4/3)*π*r^3
+
+
+
+
